667fd1856ac31c4a73154c2b9f12c8e5a89e81c5
anbit192 <minhanthongminh@gmail.com>
2025-03-12 15:19:10 +0700

    fix and improve rcm system

--- src/main.py
+++ src/main.py
@@ -1,23 +1,28 @@
 import numpy as np
-import faiss
 from config import *
 from load_data import *
 from models import *
+from datetime import datetime
+from recommender import Recommender
 
 
 movie_df = get_movie_df()
 user_rates = get_user_rates_df()
-
 index = get_embed_vectors()
 
 client = get_google_model()
+
+
+def _get_current_timestamp():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 
 def get_user_movies():
     user_dict = {}
 
     while (True):
         try:
-            input_movie_id = input("Input movie ID: ").strip()
+            input_movie_id = input("Input movie ID (exit to quit): ").strip()
             if (input_movie_id == "exit"):
                 break
             input_movie_id = int(input_movie_id)
@@ -27,27 +32,43 @@ def get_user_movies():
 
             input_rating = input("Rating of that movie: ").strip()
             input_rating = float(input_rating)
+            current_timestamp = _get_current_timestamp()
 
             if (input_rating < 0 or input_rating > 5):
                 raise ValueError("Rating must be between 0 and 5!!")
             
-            user_dict[input_movie_id] = input_rating
+            user_dict[input_movie_id] = (input_rating, current_timestamp)
         except ValueError as e:
             print(f"Invalid input: {e} Try again.")
         except Exception as e:
             print(f"Unexpected error: {e}. Try again.")
     
     id_list = list(user_dict.keys())
-    rating_list = [user_dict[id] for id in id_list]
+    rating_list = [user_dict[id][0] for id in id_list]
+    time_list = [user_dict[id][1] for id in id_list]
 
-    user_df = movie_df.loc(id_list)
+    # print(f"id list: {id_list}")
+    # print(f"ratings: {rating_list}")
+
+    user_df = movie_df.loc[id_list]
     user_df["rating"] = rating_list
+    user_df["timestamp"] = time_list
 
-    return user_df
+    return user_df[["vectorID", "title", "genres", "tags", "rating", "weight_rating", "timestamp", "page_content"]]
+
+
 
 
 def main():
-    pass
+    movie_list = get_user_movies()
+    
+    rec = Recommender.get_instance(index=index, movie_list=movie_list, output_k=20)
+    print(rec._get_rcm_ranking())
+    top_k = rec.get_top_k()
+
+    print(movie_df.iloc[top_k][["title", "genres"]])
+
+    print("Done")
 
 
 if __name__ == "__main__":
---
+++ src/recommender.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+class Recommender:
+    _instance = None
+
+    def __init__(self, index, movie_list, k_per_item=5, output_k=10, negative_thres=3, negative_alpha=-1):
+        self.index = index
+        self.movie_list = movie_list
+        self.k_per_item = k_per_item + 1
+        self.output_k = output_k
+        self.negative_thres = negative_thres
+        self.negative_alpha = negative_alpha
+
+    @classmethod
+    def get_instance(cls, index, movie_list, k_per_item=5, output_k=10, negative_thres=3, negative_alpha=-1):
+        if (cls._instance is None):
+            cls._instance = cls(index, movie_list, k_per_item, output_k, negative_thres, negative_alpha)
+        return cls._instance
+
+    def _similar_search(self, movie):
+        embed_input = self.index.reconstruct(movie["vectorID"])
+        D, I = self.index.search(np.array([embed_input]), self.k_per_item)
+
+        return D, I
+    
+    def _get_rcm_ranking(self):
+        user_dislike = []
+        user_like = []
+        recommend_dict = {}
+
+        for movieId, row in self.movie_list.iterrows():
+
+            m_rating = row["rating"]
+            m_weight_rating = row["weight_rating"]
+
+            combine_rating = 0.9 * m_rating + 0.1 * m_weight_rating
+
+            D, I = self._similar_search(row)
+
+            rcm_ids = I.flatten().tolist()[1:]
+
+            rcm_dists = D.flatten().tolist()[1:]
+
+            for id_, dist_ in zip(rcm_ids, rcm_dists):
+
+                if (m_rating < self.negative_thres):
+                    if (id_ in user_like):
+                        continue
+                    
+                    user_dislike.append(row["vectorID"])
+                    wgt = (combine_rating + self.negative_alpha *
+                        (5 - m_rating)) * dist_
+                    
+                    recommend_dict[id_] = recommend_dict.get(id_, 0) - wgt
+                else:
+                    user_like.append(row["vectorID"])
+
+                    wgt = combine_rating * dist_
+
+                    recommend_dict[id_] = recommend_dict.get(id_, 0) + wgt
+
+        
+        print(user_dislike)
+        for id_ in set(user_dislike):
+            recommend_dict.pop(id_)
+
+        sorted_candidates = sorted(recommend_dict.items(),
+                           key=lambda x: x[1], reverse=True)
+        
+        return sorted_candidates
+    
+    def set_movie_list(self, movie_list):
+        self.movie_list = movie_list
+    
+    def get_top_k(self):
+        res = self._get_rcm_ranking()
+        return [id_ for id_, _ in res[:self.output_k]]
+
+    
