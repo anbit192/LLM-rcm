6b4214d78f8922d17ac94f4966311ebdab36d340
anbit192 <minhanthongminh@gmail.com>
2025-04-04 08:36:42 +0700

    add description

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import services
 
 app = FastAPI()
 
-print("Dit me!")
+# print("Dit me!")
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,32 +21,47 @@ app.add_middleware(
 @app.get("/")
 async def hello_world():
     return {
-        "message":"Dit!"
+        "message":"Ei yoo"
     }
 
 
 @app.get("/get_movie/{id}", response_model=MovieInfosOut, status_code=status.HTTP_200_OK)
 async def get_movie_by_id(id: int):
+    """
+    Lấy ra thông tin của 1 bộ phim từ Id của phim trong database.
+    """
     return services.get_movie_by_id(id)
 
 
 @app.post("/get_movies", response_model=List[MovieInfosOut], status_code=status.HTTP_200_OK)
 async def get_movies_infos(id_list:MovieIdsList):
+    """
+    Lấy ra thông tin của nhiều bộ phim từ một list các Id có trong database.
+    """
     return services.get_movies_from_ids(id_list.movieIds)
 
 
 @app.get("/search/{query_str}", response_model=List[MovieInfosOut], status_code=status.HTTP_200_OK)
 async def search_movies(query_str):
+    """
+    Tìm kiếm các bộ phim có tên giống với query tìm kiếm.
+    """
     return services.search_movie_from_query(query_str)
 
 
 @app.post("/input_user_movies", response_model=WatchedMovie, status_code=status.HTTP_200_OK)
 async def rate_movies(rate: WatchedMovie):
+    """
+    Đánh giá 1 bộ phim. Nếu muốn đánh giá lại, có thể gửi lại cùng Id của phim đó.
+    """
     return services.rate_movie(rate)
 
 
 @app.get("/get_recommend_movies", response_model=MoviesResponse, status_code=status.HTTP_200_OK)
 async def get_recommend_movies():
+    """
+    Lấy ra 10 phim được recommend, nếu user chưa đánh giá phim nào thì sẽ hiển thị ra 10 phim mặc định của hệ thống.
+    """
     return services.recommend_movies()
 
 
--- src/validate_model.py
+++ src/validate_model.py
@@ -8,22 +8,74 @@ class MovieInfosOut(BaseModel):
     genres: List[str]
     tags: List[str]
     weight_rating: float
+    model_config = {
+            "json_schema_extra": {
+                        "examples": [
+                            {
+                                "movieId": 1,
+                                "title": "Toy Story",
+                                "genres": ["Animation", "Family"],
+                                "tags": ["kids", "family"],
+                                "weight_rating": 3.9
+                }
+            ]
+        }
+    }
 
 
 class WatchedMovie(BaseModel):
     movieId: int
-    user_rate: float
+    user_rate: float = 3.0
+    model_config = {
+            "json_schema_extra": {
+                        "examples": [
+                        {
+                                "movieId": 1,
+                                "user_rate":3.0
+                        }
+            ]
+        }
+    }
 
 class WatchedMovieOut(WatchedMovie):
     timestamp:str
+    model_config = {
+            "json_schema_extra": {
+                        "examples": [
+                        {
+                                "movieId": 1,
+                                "user_rate":3.0,
+                                "timestamp":"2025-08-19"
+                        }
+            ]
+        }
+    }
 
 
 class MovieIdsList(BaseModel):
     movieIds: List[int]
+    model_config = {
+            "json_schema_extra": {
+                        "examples": [
+                        {
+                                "movieIds": [1, 2, 3]
+                        }
+            ]
+        }
+    }
 
 
 class SearchQuery(BaseModel):
     query: str
+    model_config = {
+            "json_schema_extra": {
+                        "examples": [
+                        {
+                                "query": "toy   st",
+                        }
+            ]
+        }
+    }
 
 
 class MovieTitle(BaseModel):
@@ -34,3 +86,23 @@ class GenreMovies(BaseModel):
 
 class MoviesResponse(RootModel):
     root: List[Dict[str, List[MovieTitle]]]
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                [
+                {
+                    "Genre1": [
+                        {"title": "Movie1"},
+                        {"title": "Movie2"}
+                    ]
+                },
+                {
+                    "Genre2": [
+                        {"title": "Movie3"},
+                        {"title": "Movie2"}
+                    ]
+                }
+                ]
+            ]
+        }
+    }
