cdcd1a0d2facd63b2427a33270c2a50bb81ddcd2
anbit192 <minhanthongminh@gmail.com>
2025-04-20 08:58:56 +0700

    embed newdata

--- src/load_data.py
+++ src/load_data.py
@@ -6,14 +6,14 @@ import faiss
 parent_ = Path().cwd().parent
 
 def get_movie_df():
-    df = pd.read_csv(parent_ / "data/movie_infos_2.csv")
+    df = pd.read_csv(parent_ / "data/full_movie_infos.csv")
     return df.set_index("movieId")
 
 def get_user_rates_df():
     return pd.read_pickle(parent_ / "data/sorted_ratings.pkl")
 
 def get_embed_vectors():
-    str_ = str(parent_ / "output_index/text-embed-04")
+    str_ = str(parent_ / "output_index/text-embed-04-new-data")
     return faiss.read_index(str_)
 
 # print(get_embed_vectors().ntotal)
